bc2eea3bd168dc5bf36167da3aaf069a0a6b0119
jason-slatt <133472643+jason-slatt@users.noreply.github.com>
2024-10-26 11:29:06 +0100

    modify some settings to work well

--- e_library/settings.py
+++ e_library/settings.py
@@ -38,11 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'auth',
     'books',
     'borrowing',
-    'notifications'
-    'users'
+    'notifications',
+    'users',
+    'user_auth'
     
 ]
 
